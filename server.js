e5fc6951cae7030cf05fa3c30f0d24a40d3096a2
aaronmatson1 <matsonaaron1@gmail.com>
2020-05-13 19:34:51 -0600

    Setup server, dependencies. Run on Locoalhost:4000

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require("cors");
 
 const server = express();
+
+server.use(express.json());
+server.use(cors);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
